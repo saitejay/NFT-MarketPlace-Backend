32703e96bf6e1f731dc7671789919f8d55ff06b6
Sai Teja Yelubolu <84760544+saitejay@users.noreply.github.com>
2022-01-10 14:18:28 +0530

    Update collectionController.js
    contract_address

--- module/collection/controller/collectionController.js
+++ module/collection/controller/collectionController.js
@@ -40,7 +40,7 @@ exports.add = function(req,res) {
     collection.image = req.body.image ? req.body.image : '';
     collection.status = 1;
     collection.author_address = req.decoded.public_key;
-    collection.contract_address = req.body.collection_address;
+    collection.contract_address = req.body.contract_address;
     collection.contract_symbol = req.body.token_name;
 
     collection.save(function (err ,collectionObj) {
