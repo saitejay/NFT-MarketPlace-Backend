8208b6313861b42c0439a1b9970d620620d5ddaa
Sai Teja Yelubolu <84760544+saitejay@users.noreply.github.com>
2022-01-20 15:33:46 +0530

    Update collectionModel.js
    Added collection_keyword 20-1-22 3:30 pm

--- module/collection/model/collectionModel.js
+++ module/collection/model/collectionModel.js
@@ -20,17 +20,22 @@ var collectionSchema = mongoose.Schema({
         unique:[true, 'Collection_id already exists. Please try a different collection_id'],
         required: [true, 'Collection_id is required']
     },
+    collection_keyword:{
+        type: String,
+        unique:[true, 'collection_keyword already exists. Please try a different collection_keyword'],
+        required: [true, 'collection_keyword is required']
+    },
     name: {
         type: String,
         minlength: [3, 'Name must be 3 characters or more'],
         maxlength: [255, "Name can't exceed 255 characters"],
         unique: [ true , 'Name already exists. Please try a different name'],
         required: [ true , 'Name is required'], 
-    },   
+    },
     description: {
         type: String,
         maxlength: [1000, "Description can't exceed 1000 characters"]
-    },   
+    },
     contract_symbol:{
         type:String,
         unique: [ true, 'contract_symbol already exists. Please try a different contract_symbol'],
@@ -43,7 +48,7 @@ var collectionSchema = mongoose.Schema({
     },
     banner: {
         type: String,
-    }, 
+    },
     image: {
         type: String,
     },
