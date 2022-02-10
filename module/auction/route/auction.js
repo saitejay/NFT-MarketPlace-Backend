113f58a1cfbbdf5b269a85b6c834fb36e562c6a1
Sai Teja Yelubolu <Saitejayelubolu.1@gmail.com>
2022-02-10 18:46:09 +0530

    Added minimum_bid_amount

--- module/auction/route/auction.js
+++ module/auction/route/auction.js
@@ -1,10 +1,6 @@
 /*
-Project : Cryptotrades
+Project : NFT-marketplace
 FileName : item.js
-Author : LinkWell
-File Created : 21/07/2021
-CopyRights : LinkWell
-Purpose : This is the file which used to define all route releated to collecion api request.
 */
 
 var express = require('express')
@@ -16,7 +12,7 @@ var auctionController = require("./../controller/auctionController");
 const { check } = require('express-validator');
 
 router.get('/',auctionController.test)
-router.post('/add', [check('auction_id').not().isEmpty(), check('item_id').not().isEmpty(), check('auction_start_time').not().isEmpty(), check('auction_end_time').not().isEmpty(), auth], auctionController.add)
+router.post('/add', [check('auction_id').not().isEmpty(), check('item_id').not().isEmpty(), check('minimum_bid_amount').not().isEmpty(), check('auction_start_time').not().isEmpty(), check('auction_end_time').not().isEmpty(), auth], auctionController.add)
 router.get('/livelist', auctionController.auctionLiveList)
 router.get('/details', auctionController.details)
 router.get('/listall', auctionController.ListAllAuctions)
