d60d4f1443a822823d692892ea4880f2fbade7ad
Sai Teja Yelubolu <Saitejayelubolu.1@gmail.com>
2022-02-21 19:30:10 +0530

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /*
-/*
 Project : NFT-marketplace
 FileName : index.js
 */
@@ -27,15 +26,15 @@ global.__basedir = __dirname;
 // app.use(express.json({limit: '50mb'}));
 // app.use(express.urlencoded({limit: '50mb'}));
 app.use(
-    bodyParser.json({
-        limit: "100mb",
-    })
+  bodyParser.json({
+    limit: "100mb",
+  })
 );
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-        limit: "100mb",
-    })
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "100mb",
+  })
 );
 app.use(express.static(__dirname + "/media"));
 app.use(cors());
@@ -49,38 +48,38 @@ app.use(cors());
  * Below lines used to connect databse moongoose ORM
  */
 try {
-    mongoose.connect(
-        "mongodb+srv://" +
-            config.db.username +
-            ":" +
-            config.db.password +
-            "@cluster0.ll6t4.mongodb.net/" +
-            config.db.name,
-        {
-            keepAlive: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        }
-    );
-
-    var db = mongoose.connection;
-    // Added check for DB connection
-    if (!db) {
-        console.log("Error connecting db");
-    } else {
-        console.log("Db connected successfully");
+  mongoose.connect(
+    "mongodb+srv://" +
+      config.db.username +
+      ":" +
+      config.db.password +
+      "@cluster0.ll6t4.mongodb.net/" +
+      config.db.name,
+    {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
     }
+  );
+
+  var db = mongoose.connection;
+  // Added check for DB connection
+  if (!db) {
+    console.log("Error connecting db");
+  } else {
+    console.log("Db connected successfully");
+  }
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
 /*
  * Below lines used to define route for the api services
  */
-app.get("/", (req, res) => res.send("Welcome to Cryptotrades API"));
+app.get("/", (req, res) => res.send("Welcome to NFT Marketplace API"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/user", user);
 app.use("/settings", settings);
@@ -94,17 +93,16 @@ app.use("/auction", auction);
  * Below lines used to handle invalid api calls
  */
 app.use(function (req, res, next) {
-    res.status(404).send("Sorry can't find that!");
+  res.status(404).send("Sorry can't find that!");
 });
 app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send("Something broke!");
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 /*
  * Below lines used to run api service
  */
-//https.createServer(options, app).listen(config.app.port, () => console.log(`Cryptotrades app listening on port ${config.app.port}!`));
 app.listen(config.app.port, () =>
-    console.log(`Cryptotrades app listening on port ${config.app.port}!`)
+  console.log(`NFT Marketplace app listening on port ${config.app.port}!`)
 );
